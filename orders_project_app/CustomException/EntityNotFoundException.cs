dcb338bdb92addb16103489f7e362d194fb2a994
DAlejandroB <diegoaenciso76@gmail.com>
2025-02-27 06:41:12 -0500

    Add EntityNotFoundException class in new CustomException namespace

---
+++ orders_project_app/CustomException/EntityNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace orders_project_app.CustomException
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
