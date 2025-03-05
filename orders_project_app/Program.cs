eb91a1e4fa1f158b4935984975df5489558a961b
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-05 14:31:20 -0500

    Add DI for repositories and services, enable controllers
    - Added using directives for repository and service namespaces in `Program.cs`.
    - Registered `IOrderRepository` and `OrderRepository` with `AddScoped`.
    - Registered `IOrderService` and `OrderService` with `AddScoped`.
    - Enabled controllers with `AddControllers` method.
    - Replaced `app.MapGet("/")` with `app.MapControllers()`.

--- orders_project_app/Program.cs
+++ orders_project_app/Program.cs
@@ -1,14 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using orders_project_app.Data;
+using orders_project_app.Repository.Implementation;
+using orders_project_app.Repository.Interface;
+using orders_project_app.Service.Implementation;
+using orders_project_app.Service.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<OrdersAppDb>(options => options.UseSqlServer(connectionString));
 
+// Add repositories
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+// Add services
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
