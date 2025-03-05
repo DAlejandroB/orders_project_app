c9795d12d53b4a7faeb7f2bcb27603910e0ebebb
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-05 14:30:07 -0500

    Add OrderController with CRUD endpoints
    Introduce OrderController in OrderController.cs within the orders_project_app.Controller namespace. This RESTful API controller, decorated with [ApiController] and [Route("api/[controller]")], handles order-related operations.

---
+++ orders_project_app/Controller/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using orders_project_app.Model;
+using orders_project_app.Service.Interface;
+
+namespace orders_project_app.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController(IOrderService orderService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<Order>> GetOrders()
+        {
+            return await orderService.GetOrders();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Order> GetOrderById(int id)
+        {
+            return await orderService.GetOrderById(id);
+        }
+
+        [HttpPost]
+        public async Task AddOrder(Order order)
+        {
+            await orderService.AddOrder(order);
+        }
+
+        [HttpPut]
+        public async Task UpdateOrder(Order order)
+        {
+            await orderService.UpdateOrder(order);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteOrder(int id)
+        {
+            await orderService.DeleteOrder(id);
+        }
+    }
+}
