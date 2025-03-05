4d5c2c9a869ff84158c23a6c23ad2531a3ac1ea8
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-05 14:28:50 -0500

    Add OrderService and IOrderService for order management
    Introduce OrderService class in orders_project_app.Service.Implementation namespace, implementing IOrderService interface.

---
+++ orders_project_app/Service/Implementation/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using orders_project_app.Model;
+using orders_project_app.Repository.Interface;
+using orders_project_app.Service.Interface;
+
+namespace orders_project_app.Service.Implementation
+{
+    public class OrderService(IOrderRepository orderRepository) : IOrderService
+    {
+        public async Task<List<Order>> GetOrders()
+        {
+            return await orderRepository.GetOrders();
+        }
+
+        public async Task<Order> GetOrderById(int id)
+        {
+            return await orderRepository.GetOrderById(id);
+        }
+
+        public async Task AddOrder(Order order)
+        {
+            await orderRepository.AddOrder(order);
+        }
+
+        public async Task UpdateOrder(Order order)
+        {
+            await orderRepository.UpdateOrder(order);
+        }
+
+        public async Task DeleteOrder(int id)
+        {
+            await orderRepository.DeleteOrder(id);
+        }
+    }
+}
---
+++ orders_project_app/Service/Interface/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using orders_project_app.Model;
+
+namespace orders_project_app.Service.Interface
+{
+    public interface IOrderService
+    {
+        public Task<List<Order>> GetOrders();
+        public Task<Order> GetOrderById(int id);
+        public Task AddOrder(Order order);
+        public Task UpdateOrder(Order order);
+        public Task DeleteOrder(int id);
+    }
+}
