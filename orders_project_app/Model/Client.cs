451648a458d22ab4852e1511a21af8663a94675a
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-05 21:04:25 -0500

    Data annotations added to Client and Order entities

--- orders_project_app/Model/Client.cs
+++ orders_project_app/Model/Client.cs
@@ -1,12 +1,18 @@
-﻿namespace orders_project_app.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace orders_project_app.Model
 {
     public class Client
     {
         public int Id { get; set; }
+        [StringLength(255)]
         public required string Name { get; set; }
-        public string Address { get; set; }
-        public string ContactEmail { get; set; }
-        public string ContactPhone { get; set; }
-        public List<Order> Orders { get; set; }
+        [StringLength(255)]
+        public string? Address { get; set; }
+        [StringLength(255)]
+        public string? ContactEmail { get; set; }
+        [StringLength(255)]
+        public string? ContactPhone { get; set; }
+        public List<Order> Orders { get; set; } = [];
     }
 }
