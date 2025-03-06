f9c2c8a34992b528db169b08bb9ad8b2b363c100
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-06 14:36:51 -0500

    Add InsufficientStockException custom exception class

---
+++ orders_project_app/CustomException/InsufficientStockException.cs
@@ -0,0 +1,9 @@
+﻿namespace orders_project_app.CustomException
+{
+    public class InsufficientStockException : Exception
+    {
+        public InsufficientStockException(string message) : base(message)
+        {
+        }
+    }
+}
