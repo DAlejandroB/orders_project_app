a9a2ee25186bd3f6fa5b4235d4b234e924c50fb6
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-06 07:45:21 -0500

    Add StockItemService for managing stock items

---
+++ orders_project_app/Service/Implementation/StockItemService.cs
@@ -0,0 +1,69 @@
+﻿using orders_project_app.CustomException;
+using orders_project_app.Model;
+using orders_project_app.Model.Dto;
+using orders_project_app.Repository.Interface;
+using orders_project_app.Service.Interface;
+
+namespace orders_project_app.Service.Implementation
+{
+    public class StockItemService(IStockItemRepository stockItemRepository) : IStockItemService
+    {
+        public async Task<List<StockItemDto>> GetStockItems()
+        {
+            List<StockItem> stockItems = await stockItemRepository.GetStockItems();
+            List<StockItemDto> stockItemDtos = [];
+            stockItems.ForEach(stockItem =>
+            {
+                StockItemDto stockItemDto = new StockItemDto
+                {
+                    Id = stockItem.Id,
+                    Name = stockItem.Name,
+                    UnitPrice = stockItem.UnitPrice,
+                    Quantity = stockItem.Quantity
+                };
+                stockItemDtos.Add(stockItemDto);
+            });
+            return stockItemDtos;
+        }
+
+        public async Task<StockItemDto> GetStockItemById(int id)
+        {
+            StockItem stockItemEntity = await stockItemRepository.GetStockItemById(id);
+            StockItemDto stockItemDto = new StockItemDto
+            {
+                Id = stockItemEntity.Id,
+                Name = stockItemEntity.Name,
+                UnitPrice = stockItemEntity.UnitPrice,
+                Quantity = stockItemEntity.Quantity
+            };
+            return stockItemDto;
+        }
+
+        public async Task AddStockItem(StockItemCreateDto stockItem)
+        {
+            StockItem stockItemEntity = new StockItem
+            {
+                Name = stockItem.Name,
+                UnitPrice = stockItem.UnitPrice,
+                Quantity = stockItem.Quantity
+            };
+            await stockItemRepository.AddStockItem(stockItemEntity);
+        }
+
+        public async Task UpdateStockItem(StockItemCreateDto stockItem)
+        {
+            StockItem stockItemEntity = new StockItem
+            {
+                Name = stockItem.Name,
+                UnitPrice = stockItem.UnitPrice,
+                Quantity = stockItem.Quantity
+            };
+            await stockItemRepository.UpdateStockItem(stockItemEntity);
+        }
+
+        public async Task DeleteStockItem(int id)
+        {
+            await stockItemRepository.DeleteStockItem(id);
+        }
+    }
+}
---
+++ orders_project_app/Service/Interface/IStockItemService.cs
@@ -0,0 +1,14 @@
+﻿using orders_project_app.Model;
+using orders_project_app.Model.Dto;
+
+namespace orders_project_app.Service.Interface
+{
+    public interface IStockItemService
+    {
+        public Task<List<StockItemDto>> GetStockItems();
+        public Task<StockItemDto> GetStockItemById(int id);
+        public Task AddStockItem(StockItemCreateDto order);
+        public Task UpdateStockItem(StockItemCreateDto order);
+        public Task DeleteStockItem(int id);
+    }
+}
