2426a2195d9eba1a9820d936a390b09c42945084
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-06 07:50:41 -0500

    Added StrockItemController for API endpoint exposure

---
+++ orders_project_app/Controller/StockItemController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using orders_project_app.Model.Dto;
+using orders_project_app.Service.Interface;
+
+namespace orders_project_app.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockItemController(IStockItemService stockItemService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<StockItemDto>> GetStockItems()
+        {
+            return await stockItemService.GetStockItems();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<StockItemDto> GetStockItemById(int id)
+        {
+            return await stockItemService.GetStockItemById(id);
+        }
+
+        [HttpPost]
+        public async Task AddStockItem(StockItemCreateDto stockItem)
+        {
+            await stockItemService.AddStockItem(stockItem);
+        }
+
+        [HttpPut]
+        public async Task UpdateStockItem(StockItemCreateDto stockItem)
+        {
+            await stockItemService.UpdateStockItem(stockItem);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteStockItem(int id)
+        {
+            await stockItemService.DeleteStockItem(id);
+        }
+    }
+}
