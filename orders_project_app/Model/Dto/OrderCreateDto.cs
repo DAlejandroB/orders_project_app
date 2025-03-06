41c9fb02419fd2197fc0dad90307aa31afc025c6
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-06 07:41:30 -0500

    Removed calculate field from OrderCreateDto and refactored to reference OrderItemDto insted of purely referencing orderItemId

--- orders_project_app/Model/Dto/OrderCreateDto.cs
+++ orders_project_app/Model/Dto/OrderCreateDto.cs
@@ -5,9 +5,8 @@ namespace orders_project_app.Model.Dto
 {
     public class OrderCreateDto
     {
-        public List<int> OrderItems { get; set; } = [];
+        public List<OrderItemDto> OrderItems { get; set; } = [];
         [Precision(18, 2)]
-        public decimal TotalPrice { get; set; }
         public int ClientId { get; set; }
         public OrderStatus Status { get; set; }
         public OrderType Type { get; set; }
