a635ec21255f670834dfec9cf7377692e8e3236b
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-06 07:40:06 -0500

    Added data annotations to Item and OrderItem model entities

--- orders_project_app/Model/Item.cs
+++ orders_project_app/Model/Item.cs
@@ -3,7 +3,7 @@
     public class Item
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public int Quantity { get; set; }
     }
 }
