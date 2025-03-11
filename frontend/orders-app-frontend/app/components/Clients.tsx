d9b1ad4a566c8dfa4f60d6b77d8d4fb415f55480
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-11 08:49:46 -0500

    Client type removed from file and imported from types/client.ts Removed unnecessary styling

--- frontend/orders-app-frontend/app/components/Clients.tsx
+++ frontend/orders-app-frontend/app/components/Clients.tsx
@@ -1,15 +1,6 @@
 "use client";
 import { useEffect, useState } from "react"
-
-interface Client{
-  id: number,
-  name: string,
-  address: string,
-  contactEmail: string,
-  contactPhone: string,
-  orders: []
-
-}
+import { Client } from "../types/client";
 
 function ClientsFetch(){
   const [data, setData] = useState<Array<Client> | null>(null);
@@ -35,23 +26,23 @@ function ClientsFetch(){
   if(loading) return <p>Loading...</p>
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div>
+      <table>
+        <thead>
           <tr>
-            <th scope="col" className="py-4 px-4">Name</th>
-            <th scope="col" className="py-4 px-4">Address</th>
-            <th scope="col" className="py-4 px-4">Contact Email</th>
-            <th scope="col" className="py-4 px-4">Contact Phone</th>
+            <th scope="col">Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Contact Email</th>
+            <th scope="col">Contact Phone</th>
           </tr>
         </thead>
         <tbody>
           {data?.map((row) => (
-            <tr key={row.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{row.name}</th>
-              <td className="px-4 py-4">{row.address}</td>
-              <td className="py-4 px-4">{row.contactEmail}</td>
-              <td className="py-4 px-4">{row.contactPhone}</td>
+            <tr key={row.id}>
+              <th scope="row">{row.name}</th>
+              <td>{row.address}</td>
+              <td>{row.contactEmail}</td>
+              <td>{row.contactPhone}</td>
             </tr>
           ))}
         </tbody>
