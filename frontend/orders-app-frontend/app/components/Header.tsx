10b1fb80ac7d15f83403bd9ac735201d94391ecd
DAlejandroB <diegoaenciso76@gmail.com>
2025-03-11 08:50:57 -0500

    Update header component with new title and added link to manage clients

--- frontend/orders-app-frontend/app/components/Header.tsx
+++ frontend/orders-app-frontend/app/components/Header.tsx
@@ -1,5 +1,6 @@
 export default function Header(){  
   return <div>
-    <h1 className="text-3x1 font-bold underline">This is a Header!</h1>
+    <h1>My app!</h1>
+    <a href="/clients"> Manage Clients</a>
   </div>
 }
